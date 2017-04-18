4438eef02b3e0b4de104fb54ced8ecb1479a0a02
songjian <1015172104@qq.com>
2017-04-18 14:39:43 +0800

    first commit

---
+++ src/com/song/ssh/service/UserManager.java
@@ -0,0 +1,9 @@
+package com.song.ssh.service;
+
+import com.song.ssh.beans.UserForm;  
+
+public interface UserManager {  
+  
+    public void regUser(UserForm user);  
+  
+}  
---
+++ src/com/song/ssh/serviceImpl/UserManagerImpl.java
@@ -0,0 +1,14 @@
+package com.song.ssh.serviceImpl;
+
+import com.song.ssh.beans.UserForm;
+import com.song.ssh.service.UserManager;
+
+public class UserManagerImpl implements UserManager {
+
+	@Override
+	public void regUser(UserForm user) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
